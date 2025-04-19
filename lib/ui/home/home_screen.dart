0d3d19943379f055de458e86fa641467637562b5
viniciusloiolajg <viniciusloiolajg@gmail.com>
2025-04-19 21:46:52 +0000

    AulaO2-Logo&EscolhaPorCateg.

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -5,6 +5,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            Center(child: Image.asset('assets/logo.png', width: 147)),
+            Text('Boas-vindas!'),
+            TextFormField(),
+            Text('Escolha por categoria'),
+          ],
+        ),
+      ),
+    );
   }
 }
