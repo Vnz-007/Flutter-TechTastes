ec4eda2dd0a934006d83bf12795ee568b0bea4a3
viniciusloiolajg <viniciusloiolajg@gmail.com>
2025-04-21 16:49:55 +0000

    AulaO3-bag_provider.dart-semBugs&debugSymbolFalse

--- lib/ui/restaurant/restaurant_screen.dart
+++ lib/ui/restaurant/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/model/dish.dart';
 import 'package:myapp/model/restaurant.dart';
 
 class RestaurantScreen extends StatelessWidget {
@@ -19,7 +20,22 @@ class RestaurantScreen extends StatelessWidget {
               'Mais pedidos',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
             ),
-            Column(),
+            Column(
+              children: List.generate(restaurant.dishes.length, (index) {
+                Dish dish = restaurant.dishes[index];
+                return ListTile(
+                  onTap: () {},
+                  leading: Image.asset(
+                    'assets/dishes/default.png',
+                    width: 48.0,
+                    height: 48.0,
+                  ),
+                  title: Text(dish.name),
+                  subtitle: Text('R\$${dish.price.toStringAsFixed(2)}'),
+                  trailing: IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                );
+              }),
+            ),
           ],
         ),
       ),
